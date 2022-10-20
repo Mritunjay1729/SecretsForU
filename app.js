e855d4f80a9a0af6253a15ba26af7d61dda0a07d
Mritunjay1729 <77975156+Mritunjay1729@users.noreply.github.com>
2022-10-20 10:43:52 +0530

    Main branch

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require("mongoose-findorcreate"); //For Google auth2.0
 
 //Just testing with a series of encryptions/hash functions
 // const encrypt = require("mongoose-encryption");
@@ -43,10 +46,14 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 //Create a Schema
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate); //For Google auth2.0
 
 // //Add encryption to our schema
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
@@ -56,26 +63,74 @@ const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+//Authentication using Google
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//Authentication using Facebook
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/secrets", function(req, res) {
-  if(req.isAuthenticated()) {
-    //If Authenticated, go to secrets page
-    res.render("secrets");
-  }else{
-    //If not authenticated, redirect to login page
-    res.redirect("/login");
-  }
-})
+//Third-party authentication route
+//Google
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+//Google OAuth2.0(Third Party) redirect UJYB6wFSJE3G00nEivR5rgWp8c2xXvJ3OPWPhmuteU0IKj8nKbG3DrjiOmLwpnHGYWAVwA69zmTm
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  });
+
+
+//Facebook
+app.get('/auth/facebook',
+  passport.authenticate('facebook', {scope: ['public_profile', 'email']}));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', {
+    successRedirect: '/secrets',
+    failureRedirect: '/login'
+  }));
 
 app.get("/register", function(req, res) {
-  res.render("register");
-});
+    res.render("register");
+  });
 
 app.post("/register", function(req, res) {
   User.register({username: req.body.username}, req.body.password, function(err, user) {
@@ -102,6 +157,50 @@ app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
+
+app.get("/secrets", function(req, res) {
+  if(req.isAuthenticated()) {
+    //If Authenticated, go to secrets page
+    User.find({"secret": {$ne : null}}, function(err, foundUsers) {
+      if(err) {
+        console.log(err);
+      } else {
+        res.render("secrets", {usersWithSecrets : foundUsers});
+      }
+    });
+  }else{
+    //If not authenticated, redirect to login page
+    res.redirect("/login");
+  }
+})
+
+app.get("/submit", function(req, res) {
+  if(req.isAuthenticated()) {
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  User.findOne({_id: req.user._id}, function(err, foundUser) {
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save();
+        res.redirect("secrets");
+      }else{
+        console.log("User does not exist!");
+        res.redirect("/login");
+      }
+    }
+  })
+});
+
 
 //This is function meant for bcrypt - We have updated to Passport hashing, salting and authentication
 // app.post("/login", function(req, res) {
